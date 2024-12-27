8dc6af8e1c876c0ca422bb614228df2e31515dde
rjgabel2 <98432212+rjgabel@users.noreply.github.com>
2024-12-27 13:04:48 -0800

    Preliminary channel 2

--- apu.c
+++ apu.c
@@ -14,9 +14,18 @@ static u8 ch1_get_sample(GameBoy* gb) {
     return WAVEFORMS[gb->ch1_duty][gb->ch1_index];
 }
 
+static u8 ch2_get_sample(GameBoy* gb) {
+    if (!gb->ch2_timer--) {
+        gb->ch2_timer = gb->ch2_period;
+        gb->ch2_index = (gb->ch2_index + 1) & 7;
+    }
+    return WAVEFORMS[gb->ch2_duty][gb->ch2_index];
+}
+
 void render_audio_sample(GameBoy* gb) {
     u8 ch1_sample = ch1_get_sample(gb);
+    u8 ch2_sample = ch2_get_sample(gb);
 
-    s16 converted = ch1_sample ? -0x8000 : 0x7FFF;
+    s16 converted = (ch1_sample + ch2_sample) * 0x3FFF;
     play_sample(converted, converted);
 }
